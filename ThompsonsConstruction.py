0257eecc8af813be150b060b714d99be458ba866
Jakecw97 <g00349377@gmit.ie>
2019-03-30 18:10:55 +0000

    Initial Commit
    Created ThompsonsConstruction.py
    Added opening comment

---
+++ ThompsonsConstruction.py
@@ -0,0 +1,4 @@
+#G00349377
+#Jake Warren 
+#Graph Theory Project 2019
+#This program will allow the user to parse a Regular Expression(RE) into a Non Finite Automata(NFA). It will also check the NFA to see if the RE matches a string of text.
